967235dd4c3c0347e0a46653553faead0f035522
aaradhyabansal <aaradhyabansal21@gmail.com>
2025-01-09 18:25:04 +0530

    added day1 code into a file and pushed again

---
+++ Day1/src/main/java/com/example/myApp/Laptop.java
@@ -0,0 +1,12 @@
+package com.example.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile()
+    {
+        System.out.println("Compile Laptop 404 bugs not found");
+    }
+}
