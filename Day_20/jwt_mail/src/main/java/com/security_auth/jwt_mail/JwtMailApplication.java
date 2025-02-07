9eb2ed8947a9856f2fb71d0cb1da1c4dcc287d54
aaradhyabansal <aaradhyabansal21@gmail.com>
2025-02-07 11:31:33 +0530

    added a spring security+mail sender project

---
+++ Day_20/jwt_mail/src/main/java/com/security_auth/jwt_mail/JwtMailApplication.java
@@ -0,0 +1,13 @@
+package com.security_auth.jwt_mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtMailApplication.class, args);
+	}
+
+}
---
+++ Day_20/jwt_mail/src/test/java/com/security_auth/jwt_mail/JwtMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security_auth.jwt_mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
